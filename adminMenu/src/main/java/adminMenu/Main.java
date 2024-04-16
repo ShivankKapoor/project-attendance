26905385bc9e8ad8bb27824a9fc91ba4810d9c33
Aech_win <r.hruday@tuta.io>
2024-04-15 21:26:52 -0500

    added backend api for password

--- adminMenu/src/main/java/adminMenu/Main.java
+++ adminMenu/src/main/java/adminMenu/Main.java
@@ -1,10 +1,20 @@
 package adminMenu;
 
+import adminMenu.dbConnection.password;
+import record.Records;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Console x = new Console();
+
+        password password = new password();
+
+        Records.professorCheckin professorCheckin = new Records.professorCheckin("0", "test", 5);
+
+        password.addDailyPassword(professorCheckin);
+
     }
 }
