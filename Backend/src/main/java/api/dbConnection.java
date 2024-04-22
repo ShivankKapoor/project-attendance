9b6596ca448f7a267c5b609612fe03258354ce56
Hiran Tangella <hmtangella@gmail.com>
2024-04-22 12:52:08 -0500

    added comments

--- Backend/src/main/java/api/dbConnection.java
+++ Backend/src/main/java/api/dbConnection.java
@@ -1,8 +1,10 @@
 package api;
 import record.Records;
-
 import java.sql.*;
+import java.util.*;
 import java.time.LocalDateTime;
+import java.util.Date;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class dbConnection {
@@ -61,11 +63,14 @@ public class dbConnection {
 
         Connection conn = null;
         boolean isInClass=false;
+        int numOfAttendance=0;
+        String timing="";
         try {
             conn = DriverManager.getConnection(JDBCConnectionString);
 
             //count holds the return value if the function isUserInClass which returns whether the student exists in the class or not
             isInClass= isUserInClass(checkIn.courseId(), checkIn.utdId(), checkIn.netId());
+            startDateTiming(checkIn.courseId());
 
             //if return value is false, then the student is not in class and the student checkin is not recorded
             if(!isInClass){
@@ -73,6 +78,8 @@ public class dbConnection {
                 //returning the value failedSucceeded=0 so that we know that the checkin failed
                 return false;
             }
+
+            numOfAttendance=numberAttendance(checkIn.courseId(), checkIn.utdId());
 
             //preparing statement to execute to enter the values of the student for the course and date
             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO `seniorProject`.`checkIn` (`courseId`, `utdId`, `netId`, `time`) VALUES (?, ?, ?, ?)");
@@ -150,6 +157,104 @@ public class dbConnection {
         //returns True if student is in the class, or false if the student is not
         return isInClass;
     }
+
+    public int numberAttendance(String courseId, int utdId){
+
+        Connection conn = null;
+        int count=-1;
+
+        try {
+            conn = DriverManager.getConnection(JDBCConnectionString);
+
+            //getting count from the class with the utdid and courseid
+            PreparedStatement getCountAttendance = conn.prepareStatement("SELECT count(*) FROM `seniorProject`.`checkIn` where  `courseId`=? and `utdId`=?;");
+            getCountAttendance.setString(1, courseId);
+            getCountAttendance.setInt(2, utdId);
+
+            //saving the return value for number of attended days for the class
+            ResultSet isCourseAttendance = getCountAttendance.executeQuery();
+
+            if(isCourseAttendance.next()){
+                count = isCourseAttendance.getInt(1);
+            }
+        }
+        catch(SQLException ex) {
+            //catch for any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        }
+
+        System.out.println("attendance:"+ count);
+        return count;
+
+    }
+
+    public String startDateTiming(String courseId){
+
+        Connection conn = null;
+        String timing="";
+
+        try {
+            conn = DriverManager.getConnection(JDBCConnectionString);
+
+
+            java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+            System.out.println("current date "+ date);
+            PreparedStatement getStartTiming = conn.prepareStatement("SELECT `startDateTime` FROM `seniorProject`.`class` WHERE `classId`=? AND date(seniorProject.class.startDateTime)=?;");
+            getStartTiming.setString(1, courseId);
+            getStartTiming.setDate(2, date);
+            ResultSet ClassStartTime = getStartTiming.executeQuery();
+
+            if(ClassStartTime.next()){
+                timing = ClassStartTime.getString(1);
+            }
+        }
+        catch(SQLException ex) {
+
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        }
+
+        System.out.println("the date and timing:"+ timing);
+        return timing;
+
+    }
+    public String timeWindow(String courseId){
+
+        Connection conn = null;
+        String timingWindow="";
+
+        try {
+            conn = DriverManager.getConnection(JDBCConnectionString);
+
+
+            java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+            System.out.println("current date "+ date);
+            PreparedStatement getStartTiming = conn.prepareStatement("SELECT `timeMargin` FROM `seniorProject`.`class` WHERE `classId`=? AND date(seniorProject.class.startDateTime)=?;");
+            getStartTiming.setString(1, courseId);
+            getStartTiming.setDate(2, date);
+            ResultSet timeWindowIs = getStartTiming.executeQuery();
+
+            if(timeWindowIs.next()){
+                timingWindow = timeWindowIs.getString(1);
+            }
+        }
+        catch(SQLException ex) {
+
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        }
+
+        System.out.println("the date and timing:"+ timingWindow);
+        return timingWindow;
+
+    }
+
 }
 
-//SELECT count(*) as attendance FROM seniorProject.checkIn where utdId=12332 and courseId=0;
