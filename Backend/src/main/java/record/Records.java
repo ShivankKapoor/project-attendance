c3d8318b302bb63170ad3e4ace12015602294c23
Aech_win <r.hruday@tuta.io>
2024-04-29 23:12:30 -0500

    updated checkin endpoint

--- Backend/src/main/java/record/Records.java
+++ Backend/src/main/java/record/Records.java
@@ -3,7 +3,7 @@ package record;
 import java.util.List;
 
 public record Records (List<Records> recordsList) {
-    public record Checkin(String courseId, int utdId, String netId) { }
+    public record Checkin(String courseId, int utdId, String netId, String password) { }
 
     public record register(String fullName, String netId, String utdId, String password) { }
 
